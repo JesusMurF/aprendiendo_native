2e8f8cad246b7ca59b737a9ffe7d08f94babc4f4
Jesus Mur Fontanals <murfontanals@gmail.com>
2019-02-11 20:30:15 +0100

    Flexbox and Styles

--- App.js
+++ App.js
@@ -8,39 +8,15 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View, Image } from "react-native";
-
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
-
-class Gato extends Component {
-  render() {
-    return <Text style={styles.gato}>Hola soy {this.props.nombre}!</Text>;
-  }
-}
+import { StyleSheet, View } from "react-native";
 
 export default class App extends Component {
-  state = {
-    saludo: "Hola mundo!",
-    nombre: "Gato con botas"
-  };
-
   render() {
     return (
       <View style={styles.container}>
-        <Gato nombre={this.state.nombre} />
-        <Image
-          style={{ width: 300, height: 300 }}
-          source={require("./assets/gato-asustado.png")}
-        />
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        <Text style={styles.holaMundo}>{this.state.saludo}</Text>
+        <View style={styles.box1} />
+        <View style={styles.box2} />
+        <View style={styles.box3} />
       </View>
     );
   }
@@ -49,27 +25,22 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#ddeeff"
+    flexDirection: "row",
+    justifyContent: "space-between"
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10,
-    color: "red"
+  box1: {
+    height: 120,
+    width: 100,
+    backgroundColor: "red"
   },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
+  box2: {
+    height: 120,
+    width: 100,
+    backgroundColor: "blue"
   },
-  holaMundo: {
-    color: "red",
-    fontSize: 40
-  },
-  gato: {
-    marginBottom: 40,
-    fontSize: 40
+  box3: {
+    height: 120,
+    width: 100,
+    backgroundColor: "green"
   }
 });
