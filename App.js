21bb294743c69361f40be5c85cd5c27f75b5f343
Jesus Mur Fontanals <murfontanals@gmail.com>
2019-03-07 19:18:37 +0100

    Add touchables

--- App.js
+++ App.js
@@ -8,39 +8,28 @@
  */
 
 import React, { Component } from "react";
-import { StyleSheet, View, Text, TextInput, AlertIOS } from "react-native";
+import { StyleSheet, View, Button, Alert, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback, Text } from "react-native";
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleTextChange = this.handleTextChange.bind(this);
-    this.handleSubmitText = this.handleSubmitText.bind(this);
-  }
-
-  state = {
-    texto: "Cual es tu nombre"
-  };
-
-  handleTextChange(texto) {
-    this.setState({
-      texto
-    });
-  }
-
-  handleSubmitText() {
-    AlertIOS.alert('Tu nombre es ', `${this.state.texto}!!` );
-  }
-
   render() {
     return (
       <View style={styles.container}>
-        <TextInput
-          placeholder="¿Como te llamas...?"
-          onChangeText={this.handleTextChange}
-          onSubmitEditing={this.handleSubmitText}
+        <Button
+          onPress={() => {
+          Alert.alert('Button');
+          }}
+          title="Button"
         />
-        <Text style={styles.texto}>{this.state.texto}</Text>
+        <TouchableHighlight style={styles.touchableHighlight} underlayColor={'grey'} onPress={() => Alert.alert('Touchable highlight')}>
+          <Text>My touchable highlight</Text>
+        </TouchableHighlight>
+        <TouchableOpacity style={styles.touchableOpacity} onPress={() => Alert.alert('Touchable highlight')}>
+          <Text>Touchable opacity</Text>
+        </TouchableOpacity>
+        {/* Este es el boton que menos se usa */}
+        <TouchableWithoutFeedback onPress={() => Alert.alert('Touchable without feedback')}>
+          <Text>Touchable without feedback</Text>
+        </TouchableWithoutFeedback>
       </View>
     );
   }
@@ -51,9 +40,29 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "white",
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
   texto: {
     fontSize: 20
+  },
+  touchableHighlight: {
+    backgroundColor: 'moccasin',
+    borderRadius: 50,
+    width: 300,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 10,
+    marginTop: 10
+  },
+  touchableOpacity: {
+    backgroundColor: 'deepskyblue',
+    borderRadius: 50,
+    width: 300,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 10,
+    marginTop: 10
   }
 });
